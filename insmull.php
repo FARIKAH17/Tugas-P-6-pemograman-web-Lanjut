035e679c122d919eff1d2353037ef2f75497d051
FARIKAH17 <hallosey801@gmail.com>
2025-10-29 21:10:38 +0800

    Update insmull.php

--- insmull.php
+++ insmull.php
@@ -1,1 +1,16 @@
+<?php
+include "koneksi.php";
 
+$sql  = "INSERT INTO barang (id, nama, harga, jumlah, keterangan, foto) 
+          VALUES (6, 'aglonemaRoDudAnjamani', 65000, 10, NULL, 'aglonemaRoDudAnjamani.jpg');";
+$sql .= "INSERT INTO barang (id, nama, harga, jumlah, keterangan, ) 
+          VALUES (8, 'aglonemaVenus', 65000, 10, '-', 'aglonemaVenus.');";
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
